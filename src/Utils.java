b070b1c711e76a4b29f2cbbedbeff819e4be2161
Your Name <you@example.com>
2022-12-26 02:15:11 +0300

    Класс Car дополнен, объекты откорректированы согласно изменениям класса

--- src/Utils.java
+++
@@ -1,6 +0,0 @@
-public class Utils {
-    // Метод isStringNotNullAndNotBlank(String str) проверяет не является ли строка str null и не пустая ли она.
-    public static boolean isStringNotNullAndNotBlank(String str) {
-        return str != null && !str.isBlank();
-    }
-}
